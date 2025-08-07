2f470166a53b5453295534d0344ea8aa78951cbd
Hassan0805 <malihassan1487@gmail.com>
2025-08-07 17:14:38 +0500

    created for loop

---
+++ tut10.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    /* Loops in C++
+    There are three type of loops in C++
+    1. For Loop
+    2> While Loop
+    3. do-While loop
+    */
+
+    /* For Loop in C++ */
+    //    int i=1;
+    //    cout<<i;
+    //    i++;
+
+    // for(initialization; condition; updation)
+    // {
+    //     loop body(C++ Code);
+    // }
+
+    for (int i = 0; i < 40; i++)
+    {
+        /* code */
+        cout << i <<endl;
+        
+    }
+
+    return 0;
+}
