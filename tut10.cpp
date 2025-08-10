c6982e966fd98dc0d91db385a9bdc9e54c2c1230
Hassan0805 <malihassan1487@gmail.com>
2025-08-10 11:52:05 +0500

    for, while and do while loop

--- tut10.cpp
+++ tut10.cpp
@@ -20,12 +20,63 @@ int main()
     //     loop body(C++ Code);
     // }
 
-    for (int i = 0; i < 40; i++)
-    {
-        /* code */
-        cout << i <<endl;
-        
-    }
+    // for (int i = 0; i < 21; i++)
+    // {
+    //     /* code */
+    //     cout << i <<endl;  
+    // }
+
+    // Example of infinite for loop  
+    // for (int i = 1; 14 <= 21; i++)
+    // {
+    //     /* code */
+    //     cout << i <<endl;  
+    // }
+
+    /* While Loop in C++ */
+    /* Syntax */
+    // while (condition)
+    // {
+    //     /* code */
+    //     C++ Statements;
+    // }
+
+    // Printing 1 to 40 Using While Loop 
+    // int i = 1;
+    // while (i<=40){
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+
+    // Example of infinite while loop  
+    //  int i = 1;
+    //  while(true){
+    //     cout<<i<<endl;
+    //     i++;
+    //  }
+
+    /* Do While Loop in C++ */
+    /* Syntax */
+    // do 
+    // {
+    //     /* code */
+    //     C++ Statements;
+    // }while (condition);
+
+    // Printing 1 to 40 Using do While Loop 
+    // int i = 1;
+    // do{
+    //     cout<<i<<endl;
+    //     i++;
+    // }while (false);
+
+    // Printing a table of 6 using do while loop
+    int i = 6;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=30);
+
 
     return 0;
 }
